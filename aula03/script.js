049c831622959be89ca587d6db76b2b0a5e7239d
sarahbernardino799 <sarahberna799@gmail.com>
2024-07-24 15:00:41 -0300

    atividade 03 realizada

---
+++ aula03/script.js
@@ -0,0 +1,42 @@
+
+const inputUsername = document.getElementById("input-username");
+const inputTitulo = document.getElementById("input-titulo");
+const inputPost = document.getElementById("input-post");
+const inputQtdPosts = document.getElementById("qtd-posts");
+const botaoPostar = document.getElementById("botao-postar");
+const botaoDeletar = document.getElementById("deletar");
+const listDePosts = document.getElementById("lista-de-posts");
+
+botaoPostar.addEventListener(
+
+  function postar() {
+ 
+    for (let x = 1; x <= inputQtdPosts.value; x++) {
+      const nome = inputUsername.value;
+      const titulo = inputTitulo.value;
+      const mensagem = inputPost.value;
+
+      const nomeElemento = document.createElement("p");
+      nomeElemento.textContent = nome;
+      const tituloElemento = document.createElement("h2");
+      tituloElemento.textContent = titulo;
+      const mensagemElemento = document.createElement("small");
+      mensagemElemento.textContent = mensagem;
+      const postLiElement = document.createElement("li");
+      postLiElement.appendChild(nomeElemento);
+      postLiElement.appendChild(tituloElemento);
+      postLiElement.appendChild(mensagemElemento);
+      listDePosts.appendChild(postLiElement);
+    }
+  }
+);
+
+botaoDeletar.addEventListener("click", function deletar() {
+  const ultimoItemDaLista =
+    listDePosts.children[listDePosts.children.length - 1];
+  if (ultimoItemDaLista) {
+    listDePosts.removeChild(ultimoItemDaLista);
+  } else {
+    alert("Lista Vazia");
+  }
+});
